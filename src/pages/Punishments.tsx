b28912a5c334b3d5eca5550331198181db1f1dd4
mikeyhaidinger <101424234+mikeyhaidinger@users.noreply.github.com>
2025-09-01 20:25:14 -0400

    Update current sacko image

--- src/pages/Punishments.tsx
+++ src/pages/Punishments.tsx
@@ -21,7 +21,7 @@ const Punishments = () => {
 
   const currentSacko = {
     name: 'Dave Voitek - Central Saudi Scammers',
-    image: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400&h=400&fit=crop'
+    image: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=400&h=400&fit=crop'
   };
 
   const pastSackos = [
